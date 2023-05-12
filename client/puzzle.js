698e36fab3c0b5ad19cf529227fda5719f089cc8
WintrCat <wintrcat.wilson@gmail.com>
2023-05-13 00:43:38 +0100

    I have no idea

---
+++ client/puzzle.js
@@ -0,0 +1,11 @@
+const score = document.querySelector("#score");
+const difficulty = document.querySelector("#difficulty");
+const puzzleImage = document.querySelector("#puzzleImage");
+const puzzleAnswer = document.querySelector("#puzzleAnswer");
+const puzzleSubmit = document.querySelector("#puzzleSubmit");
+
+puzzleSubmit.addEventListener("click", () => {
+    let answer = puzzleAnswer.value;
+
+
+});
